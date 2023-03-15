08206820d1289cbd3aafece959d97cbbb42179db
3v0k4 <riccardo.odone@gmail.com>
2023-03-15 21:41:12 +0100

    feat: clear search

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export const Form = ({ query: initialQuery }: { query: string }) => {
   const [query, setQuery] = useState(initialQuery);
+  const queryRef = useRef<HTMLInputElement>(null);
+  const clearSearch = () => {
+    setQuery("");
+    const element = queryRef.current;
+    if (!element) return;
+    element.focus();
+  };
 
   return (
     <form
@@ -21,36 +28,40 @@ export const Form = ({ query: initialQuery }: { query: string }) => {
         className="search__input"
         id="search__input"
         name="query"
+        ref={queryRef}
       />
 
-      <button
-        id="search__clear"
-        className="search__clear"
-        type="button"
-        aria-label="clear query"
-      >
-        <i>
-          <svg
-            className="search__icon"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            xmlSpace="preserve"
-            version="1.1"
-            viewBox="0 0 847 847"
-            x="0px"
-            y="0px"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-          >
-            <g>
-              <path
-                className="fil0"
-                d="M423 12c227,0 412,184 412,411 0,227 -185,412 -412,412 -227,0 -411,-185 -411,-412 0,-227 184,-411 411,-411zm0 469l-148 149c-38,38 -96,-20 -58,-58l148 -149 -148 -148c-38,-38 20,-96 58,-58l148 148 149 -148c38,-38 96,20 58,58l-149 148 149 149c38,38 -20,96 -58,58l-149 -149zm0 -449c-216,0 -391,175 -391,391 0,216 175,392 391,392 216,0 392,-176 392,-392 0,-216 -176,-391 -392,-391z"
-              />
-            </g>
-          </svg>
-        </i>
-      </button>
+      {query.length === 0 ? null : (
+        <button
+          id="search__clear"
+          className="search__clear"
+          type="button"
+          aria-label="clear query"
+          onClick={clearSearch}
+        >
+          <i>
+            <svg
+              className="search__icon"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              xmlSpace="preserve"
+              version="1.1"
+              viewBox="0 0 847 847"
+              x="0px"
+              y="0px"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            >
+              <g>
+                <path
+                  className="fil0"
+                  d="M423 12c227,0 412,184 412,411 0,227 -185,412 -412,412 -227,0 -411,-185 -411,-412 0,-227 184,-411 411,-411zm0 469l-148 149c-38,38 -96,-20 -58,-58l148 -149 -148 -148c-38,-38 20,-96 58,-58l148 148 149 -148c38,-38 96,20 58,58l-149 148 149 149c38,38 -20,96 -58,58l-149 -149zm0 -449c-216,0 -391,175 -391,391 0,216 175,392 391,392 216,0 392,-176 392,-392 0,-216 -176,-391 -392,-391z"
+                />
+              </g>
+            </svg>
+          </i>
+        </button>
+      )}
 
       <ul className="search__result-list"></ul>
 
